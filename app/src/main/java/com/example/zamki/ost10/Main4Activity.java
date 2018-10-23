cfff47cc36dbf5f0df42c7c69a1d9e3b72e4ebe3
daniel <danielfularczyk@gmail.com>
2018-10-23 22:56:54 +0200

    Test

--- app/src/main/java/com/example/zamki/ost10/Main4Activity.java
+++ app/src/main/java/com/example/zamki/ost10/Main4Activity.java
@@ -1,6 +1,5 @@
 package com.example.zamki.ost10;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -8,8 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    int login = 0;
+public class Main4Activity extends AppCompatActivity {
+
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -19,13 +18,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    startActivity(new Intent(MainActivity.this, MainActivity.class));
+                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    startActivity(new Intent(MainActivity.this, Main2Activity.class));
+                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    startActivity(new Intent(MainActivity.this, Main3Activity.class));
+                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -35,13 +34,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main4);
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-
-
     }
 
 }
